68e509d39e75ce229c037db90a890e19cda921b8
Diksha Jain <121768409+diksha2722@users.noreply.github.com>
2024-06-16 15:12:31 +0530

    Quiz.jsx

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,66 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Quiz.css';
 
-
-// function Quiz() {
-//   const [currentQuestion, setCurrentQuestion] = useState(0);
-//   const [score, setScore] = useState(0);
-//   const [showScore, setShowScore] = useState(false);
-//   const [quizData, setQuizData] = useState([]);
-
-//   useEffect(() => {
-//     fetch('quizData.json')
-//       .then(response => response.json())
-//       .then(data => setQuizData(data))
-//       .catch(error => console.error('Error loading quiz data:', error));
-//   }, []);
-
-//   const handleAnswer = (answer) => {
-//     const nextQuestion = currentQuestion + 1;
-//     if (nextQuestion < quizData.length) {
-//       setCurrentQuestion(nextQuestion);
-//     } else {
-//       setShowScore(true);
-//     }
-//     if (answer === quizData[currentQuestion].correctAnswer) {
-//       setScore(score + 1);
-//     }
-//   };
-
-//   const handleRestart = () => {
-//     setCurrentQuestion(0);
-//     setScore(0);
-//     setShowScore(false);
-//   };
-
-//   return (
-//     <div className="quiz">
-//       {showScore? (
-//         <div className="score-section">
-//           You scored {score} out of {quizData.length}
-//           <button onClick={handleRestart}>Restart</button>
-//         </div>
-//       ) : (
-//         <div className="question-section">
-//           <div className="question-count">
-//             <span>Question {currentQuestion + 1}</span>/{quizData.length}
-//           </div>
-//           <div className="question-text">
-//             {quizData[currentQuestion].question}
-//           </div>
-//           <div className="answer-section">
-//             {quizData[currentQuestion].answers.map((answer, index) => (
-//               <button key={index} onClick={() => handleAnswer(answer)}>
-//                 {answer}
-//               </button>
-//             ))}
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
 function Quiz() {
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [score, setScore] = useState(0);
